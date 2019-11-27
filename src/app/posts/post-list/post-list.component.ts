3dcda701a20782c1e1d53b611ee16a7d92a443f9
Nitish Aggarwal <royalnitish21@gmail.com>
2019-11-27 22:08:48 +0530

    Angular Frontend for angular mean-course

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: 'First Post', content: 'This is the first post\'s content'},
+  //   { title: 'Second Post', content: 'This is the second post\'s content'},
+  //   { title: 'Third Post', content: 'This is the second post\'s content'}
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
